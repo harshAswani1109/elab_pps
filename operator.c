81fb07c790e098fb4c500aa47cecd6eea1c2db63
harshAswani1109 <104360645+harshAswani1109@users.noreply.github.com>
2022-04-25 18:45:22 +0530

    add more operators
    add relational, bitwise, increment and decrement operator

--- operator.c
+++ operator.c
@@ -10,4 +10,22 @@ int main() {
     mod = num1 % num2;
     div = (float) num1 / num2;
     printf("Sumation : %d\nsubtraction : %d\nmultiplication : %d\ndivision : %.2f\nmodulation : %d",add,sub,mul,div,mod);
+    
+    
+    if(num1 > num2) {
+        printf("%d is greater than %d\n",num1,num2);
+    } else if (num2 > num1){
+        printf("%d is greater than %d\n",num2,num1);
+    } else {
+        printf("%d is equal to %d\n",num1,num2);
+    }
+    
+    printf("Using Inrement operator on %d : %d\n",num1,num1++);
+    printf("Using Decrement operator on %d : %d\n",num2, num2--);
+    
+    printf("Bitwise & operator : %d\n",num1 & num2);
+    printf("Bitwise | operator : %d\n",num1 | num2);
+    printf("Bitwise ~ operator : %d\n",~(num1));
+    
+    return 0;
 }
